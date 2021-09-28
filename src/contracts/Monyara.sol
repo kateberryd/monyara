5241ab053d6b32e8b25201399e787bcb88c9d55b
Thompson <Thompsonmina7@gmail.com>
2021-09-28 02:47:09 +0100

    utolized safemath librabry

--- src/contracts/Monyara.sol
+++ src/contracts/Monyara.sol
@@ -27,6 +27,68 @@ interface IERC20Token {
     );
 }
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that revert on error
+ */
+library SafeMath {
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+
+  }
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+
+  }
+
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+
+}
+
 contract Loan {
     struct LoanDetails {
         address payable borrowerAddress;
@@ -41,6 +103,8 @@ contract Loan {
         bool isPaid;
         uint256 timestamp;
     }
+    
+    using SafeMath for uint256;
 
     uint256 loanLength = 0;
     uint256 loanRegistrationAmount = 1;
@@ -155,7 +219,7 @@ contract Loan {
             IERC20Token(cUsdTokenAddress).transferFrom(
                 msg.sender,
                 bankAddress,
-                (singleLoan.amount + singleLoan.amount * 1 / 10)
+                (singleLoan.amount.add(singleLoan.amount.multiply(1 / 10)))
             ),
             "Did not reedem loan"
         );
